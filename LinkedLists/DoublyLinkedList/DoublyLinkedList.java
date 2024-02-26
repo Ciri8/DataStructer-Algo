30b3abc74526983b772ca5f828a1d9b7e0700475
Sami Ennaciri <samiennaciri@gmail.com>
2024-02-25 19:14:40 -0500

    Insert method for Doublyll and traversal

--- LinkedLists/DoublyLinkedList/DoublyLinkedList.java
+++ LinkedLists/DoublyLinkedList/DoublyLinkedList.java
@@ -20,4 +20,55 @@ public class DoublyLinkedList{
      }
 
 
+
+     public void insert(int val,int location){
+      Node node = new Node(val);
+      if (head == null){
+         createSingleDLL(val);
+         return;
+      }else if (location == 0){
+         node.next = head;
+         node.prev = null;
+         head.prev = node;//heads previous pointer is set to the node
+         head = node;//head becomes ndoe
+      }else if (location >= size){
+         node.next = null;//set to null because there is nothing after tail
+         node.prev = tail;//links nodes previous node to the tail
+         tail.next = node;//tails pointer becomes the node
+         tail = node;//tail becomes node
+      }else{
+         Node tempNode = head;
+         int index = 0;
+         while (index < location - 1){//loop to find the location of node before it to insert
+            tempNode = tempNode.next;
+            index++;
+         }
+         node.prev = tempNode;//linking previous node with current new node we made
+         node.next = tempNode.next;//linking next node to the new node we made
+         tempNode.next = node;//new nodes refrences now becomes the node
+         node.next.prev = node;//this links the current nodes next node with the node before it making it the node link.
+         
+      }
+      size++;
+  }
+//normal traversal
+  public void traverse(){
+   if (head == null){
+      System.out.print("No DLL here!");
+   }else{
+      Node tempNode = head;
+      for (int i = 0; i < size; i++){
+         System.out.print(tempNode.val);
+         if (tempNode.next != null){
+            System.out.print(" ==> ");
+         }
+         tempNode = tempNode.next;
+      }
+   }
+  }
+
+
+
 }
+
+
--- LinkedLists/DoublyLinkedList/Main.java
+++ LinkedLists/DoublyLinkedList/Main.java
@@ -3,8 +3,16 @@ package LinkedLists.DoublyLinkedList;
 public class Main {
     public static void main(String[] args) {
         DoublyLinkedList dll = new DoublyLinkedList();
-        dll.createSingleDLL(10);
-        System.out.println(dll.head.val);
+        // dll.createSingleDLL(10);
+        // System.out.println(dll.head.val);
+        dll.insert(10, 0);
+        dll.insert(20, 1);
+        dll.insert(30, 2);
+        dll.insert(40, 3);
+        dll.traverse();
+
 
     }
+
+
 }
